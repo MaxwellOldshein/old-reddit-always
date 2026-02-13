a268dfbf5edae5ba4cdfff31c80faa406cb490ce
Maxwell Oldshein <maxwell.oldshein@gmail.com>
2026-02-12 20:50:53 -0500

    Update regex to exclude Reddit gallery links

--- background.js
+++ background.js
@@ -12,7 +12,7 @@ chrome.runtime.onInstalled.addListener(() => {
                     }
                 },
                 "condition": {
-                    "regexFilter": "^https:\\/\\/(?:www\\.)?reddit\\.com((?!\\/media).*)?$",
+                    "regexFilter": "^https:\\/\\/(?:www\\.)?reddit\\.com((?!\\/media|\\/gallery).*)?$",
                     "resourceTypes": ["main_frame"]
                 }
             }
